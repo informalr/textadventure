37e0fbfd5c29867f1c36ae468c86447a171e1b05
richelbilderbeek <richel@richelbilderbeek.nl>
2020-07-14 20:24:57 +0200

    Add 'session' argument to 'get_server'

--- R/get_server.R
+++ R/get_server.R
@@ -1,8 +1,9 @@
 #' Function that returns the server part of the app
 #' @param input something
 #' @param output something
+#' @param session something
 #' @export
-get_server <- function(input, output) {
+get_server <- function(input, output, session) {
   output$text <- shiny::renderText({
     textadventure::get_room_outside_text()
   })
