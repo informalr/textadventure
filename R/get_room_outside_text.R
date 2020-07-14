5937de0d9f8d36af7a9e3205e57e5c5b2383b31d
richelbilderbeek <richel@richelbilderbeek.nl>
2020-07-14 21:05:47 +0200

    Display door

--- R/get_room_outside_text.R
+++ R/get_room_outside_text.R
@@ -1,5 +1,13 @@
 #' Get the text for the room called \code{outside}
 #' @export
 get_room_outside_text <- function() {
-  "You are outside. The moon is shining and a wolf is howling."
+  text <- "You are outside. The moon is shining and a wolf is howling."
+  is_door_open <- get("is_door_open", envir = .GlobalEnv)
+  if (isTRUE(is_door_open)) {
+    text <- c(text, "The door is open")
+  } else {
+    testthat::expect_false(is_door_open)
+    text <- c(text, "The door is closed")
+  }
+  text
 }
