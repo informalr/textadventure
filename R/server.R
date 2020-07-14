313fa5a677b29faa0da6bc3bd78e50c2ffc9293b
Willy Tadema <friesewoudloper@gmail.com>
2020-07-14 20:15:21 +0200

    Add session parameter

--- R/server.R
+++ R/server.R
@@ -1,6 +1,6 @@
 #' Function that returns the server part of the app
 #' @export
-get_server <- function(input, output) {
+get_server <- function(input, output, session) {
   output$text <- shiny::renderText({
     "The moon is shining and the wulf is howling."
   })
